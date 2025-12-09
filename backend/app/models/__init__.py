ec3c256d3ce47dc857b5f7a89d5d95ebc44b1766
kuro48 <kuroryu.basket@gmail.com>
2025-12-09 10:31:09 +0900

    update

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,16 +4,13 @@
 
 from .base import BaseModel
 from .user import User
-from .device import Device
 from .csi_data import CSIData, Session
-from .breathing_analysis import BreathingAnalysis, Alert
+from .breathing_analysis import BreathingAnalysis
 
 __all__ = [
     "BaseModel",
     "User",
-    "Device",
     "CSIData",
     "Session",
     "BreathingAnalysis",
-    "Alert"
 ]
